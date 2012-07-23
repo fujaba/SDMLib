94fdf86d8eb29ba58d2231f7af21722e9c84acd2
nethus <nethus@2b52b8e8-071c-4d2f-a868-389f512d1cbb>
2012-07-23 18:51:05 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/sdmlib/code-0/trunk@147 2b52b8e8-071c-4d2f-a868-389f512d1cbb

--- testhelpers/org/sdmlib/model/test/superclasses/creators/CreatorCreator.java
+++ testhelpers/org/sdmlib/model/test/superclasses/creators/CreatorCreator.java
@@ -1,12 +1,13 @@
 package org.sdmlib.model.test.superclasses.creators;
 
 import org.sdmlib.serialization.json.JsonIdMap;
+import org.sdmlib.serialization.json.SDMLibJsonIdMap;
 
 public class CreatorCreator
 {
    public static JsonIdMap createIdMap(String sessionID)
    {
-      JsonIdMap jsonIdMap = new JsonIdMap().withSessionId(sessionID);
+	   JsonIdMap jsonIdMap = new SDMLibJsonIdMap().withSessionId(sessionID);
       
       jsonIdMap.addCreator(new org.sdmlib.model.test.superclasses.creators.ContinentCreator());
       jsonIdMap.addCreator(new org.sdmlib.model.test.superclasses.creators.StateCreator());
