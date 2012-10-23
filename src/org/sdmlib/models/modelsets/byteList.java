d050ea23e31f6b099406acce6d1c36bb27aa2dc0
slindel <slindel@2b52b8e8-071c-4d2f-a868-389f512d1cbb>
2012-10-23 12:50:46 +0000

    add byteList
    git-svn-id: https://svn.code.sf.net/p/sdmlib/code-0/trunk@237 2b52b8e8-071c-4d2f-a868-389f512d1cbb

---
+++ src/org/sdmlib/models/modelsets/byteList.java
@@ -0,0 +1,85 @@
+/*
+   Copyright (c) 2012 zuendorf 
+   
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+   and associated documentation files (the "Software"), to deal in the Software without restriction, 
+   including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+   sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+   furnished to do so, subject to the following conditions: 
+   
+   The above copyright notice and this permission notice shall be included in all copies or 
+   substantial portions of the Software. 
+   
+   The Software shall be used for Good, not Evil. 
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+   BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
+   
+package org.sdmlib.models.modelsets;
+
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+
+import org.sdmlib.utils.PropertyChangeInterface;
+
+public class byteList extends ArrayList<Byte> implements PropertyChangeInterface
+{
+
+	private static final long serialVersionUID = -2345886837862490672L;
+
+	public byte sum()
+   {
+      byte result = 0;
+      
+      for (byte x : this)
+      {
+         result += x;
+      }
+      
+      return result;
+   }
+   
+
+   
+   //==========================================================================
+   public Object get(String attrName)
+   {
+//      int pos = attrName.indexOf('.');
+//      String attribute = attrName;
+//      
+//      if (pos > 0)
+//      {
+//         attribute = attrName.substring(0, pos);
+//      }
+      
+      return null;
+   }
+
+   
+   //==========================================================================
+   public boolean set(String attrName, Object value)
+   {
+      return false;
+   }
+
+   
+   //==========================================================================
+   protected final PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+   
+   public PropertyChangeSupport getPropertyChangeSupport()
+   {
+      return listeners;
+   }
+
+   
+   //==========================================================================
+   public void removeYou()
+   {
+      getPropertyChangeSupport().firePropertyChange("REMOVE_YOU", this, null);
+   }
+}
+
