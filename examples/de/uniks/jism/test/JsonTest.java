1a6d1ac7fe0e537bb5dea5d0ec6e2e1f28f06d0f
slindel <slindel@2b52b8e8-071c-4d2f-a868-389f512d1cbb>
2013-05-22 11:38:26 +0000

    new Test for Json
    git-svn-id: https://svn.code.sf.net/p/sdmlib/code-0/trunk@400 2b52b8e8-071c-4d2f-a868-389f512d1cbb

---
+++ examples/de/uniks/jism/test/JsonTest.java
@@ -0,0 +1,37 @@
+package de.uniks.jism.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.sdmlib.serialization.json.JsonIdMap;
+import org.sdmlib.serialization.json.JsonObject;
+
+import de.uniks.jism.test.model.ChatMessage;
+import de.uniks.jism.test.model.ChatMessageCreator;
+
+public class JsonTest {
+	@Test
+	public void testChatMessage(){
+		ChatMessage chatMessage=new ChatMessage();
+		chatMessage.setText("Dies ist eine Testnachricht");
+		chatMessage.setSender("Stefan Lindel");
+		
+		JsonIdMap jsonMap = new JsonIdMap();
+		jsonMap.withCreator(new ChatMessageCreator());
+		
+		String reference="{\r\n  \"id\":\"J1.C1\",\r\n  \"class\":\"de.uniks.jism.test.model.ChatMessage\",\r\n  \"prop\":{\r\n    \"sender\":\"Stefan Lindel\",\r\n    \"txt\":\"Dies ist eine Testnachricht\"\r\n  }\r\n}";
+		System.out.println(reference);
+		JsonObject actual=jsonMap.toJsonObject(chatMessage);
+		assertEquals("WERT Vergleichen", reference, actual.toString(2));
+		assertEquals(reference.length(), actual.toString(2).length());
+		
+		String msg = actual.toString(2);
+		
+		JsonIdMap map = new JsonIdMap();
+		map.withCreator(new ChatMessageCreator());
+		
+		ChatMessage newChatMsg = (ChatMessage) map.readJson(new JsonObject(msg));
+		System.out.println(newChatMsg);
+		
+	}
+}
