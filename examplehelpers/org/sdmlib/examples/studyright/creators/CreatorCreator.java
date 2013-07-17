23ee20c5b2b1c6998c8c7d2881d3324f61379e44
zuendorf <zuendorf@2b52b8e8-071c-4d2f-a868-389f512d1cbb>
2013-07-17 11:44:15 +0000

    generated anew
    git-svn-id: https://svn.code.sf.net/p/sdmlib/code-0/trunk@433 2b52b8e8-071c-4d2f-a868-389f512d1cbb

--- examplehelpers/org/sdmlib/examples/studyright/creators/CreatorCreator.java
+++ examplehelpers/org/sdmlib/examples/studyright/creators/CreatorCreator.java
@@ -1,55 +1,38 @@
 package org.sdmlib.examples.studyright.creators;
 
+import java.util.LinkedHashSet;
+import org.sdmlib.serialization.interfaces.SendableEntityCreator;
 import org.sdmlib.serialization.json.JsonIdMap;
 import org.sdmlib.serialization.json.SDMLibJsonIdMap;
 
 public class CreatorCreator
 {
+   public static LinkedHashSet<SendableEntityCreator> creatorSet = null;
+   
+   public static LinkedHashSet<SendableEntityCreator> getCreatorSet()
+   {
+      if (creatorSet == null)
+      {
+         creatorSet = new LinkedHashSet<SendableEntityCreator>();
+         creatorSet.add(new org.sdmlib.examples.studyright.creators.UniversityCreator());
+         creatorSet.add(new org.sdmlib.examples.studyright.creators.UniversityPOCreator());
+         creatorSet.add(new org.sdmlib.examples.studyright.creators.StudentCreator());
+         creatorSet.add(new org.sdmlib.examples.studyright.creators.StudentPOCreator());
+         creatorSet.add(new org.sdmlib.examples.studyright.creators.RoomCreator());
+         creatorSet.add(new org.sdmlib.examples.studyright.creators.RoomPOCreator());
+         creatorSet.addAll(org.sdmlib.models.pattern.creators.CreatorCreator.getCreatorSet());
+      }
+      
+      return creatorSet;
+   }
+
    public static JsonIdMap createIdMap(String sessionID)
    {
       JsonIdMap jsonIdMap = new SDMLibJsonIdMap().withSessionId(sessionID);
       
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyright.creators.UniversityCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyright.creators.StudentCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyright.creators.RoomCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyright.creators.ProfessorCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyright.creators.TopicCreator());
+      jsonIdMap.withCreator(getCreatorSet());
+
       return jsonIdMap;
    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
