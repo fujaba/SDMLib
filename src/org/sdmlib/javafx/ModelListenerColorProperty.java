803a07263bc212cdb550f9f2db03ba49a6a70920
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2013-12-16 13:23:15 +0100

    non utf8 character removed

--- src/org/sdmlib/javafx/ModelListenerColorProperty.java
+++ src/org/sdmlib/javafx/ModelListenerColorProperty.java
@@ -10,7 +10,7 @@ import org.sdmlib.serialization.interfaces.SendableEntityCreator;
  Copyright (c) 2011 - 2013, Stefan Lindel
  All rights reserved.
  
- Licensed under the EUPL, Version 1.1 or – as soon they
+ Licensed under the EUPL, Version 1.1 or later as soon they
  will be approved by the European Commission - subsequent
  versions of the EUPL (the "Licence");
  You may not use this work except in compliance with the Licence.
