9e1d7fbb48c682acb95fca9adb27928315c040b5
Christian Repik <cre@cs.uni-kassel.de>
2014-02-20 22:58:02 +0100

    fix license

--- src/org/sdmlib/serialization/sort/SortingDirection.java
+++ src/org/sdmlib/serialization/sort/SortingDirection.java
@@ -5,7 +5,7 @@ package org.sdmlib.serialization.sort;
  Copyright (c) 2011 - 2013, Stefan Lindel
  All rights reserved.
  
- Licensed under the EUPL, Version 1.1 or – as soon they
+ Licensed under the EUPL, Version 1.1 or - as soon they
  will be approved by the European Commission - subsequent
  versions of the EUPL (the "Licence");
  You may not use this work except in compliance with the Licence.
