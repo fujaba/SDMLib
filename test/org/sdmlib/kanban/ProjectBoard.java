311201b579c547eebc98459ff3fde7851adb1eb3
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2014-06-03 19:57:16 +0200

    prepared, not yet used

--- test/org/sdmlib/kanban/ProjectBoard.java
+++ test/org/sdmlib/kanban/ProjectBoard.java
@@ -24,196 +24,198 @@ package org.sdmlib.kanban;
 import java.util.LinkedHashMap;
 
 import org.junit.Test;
+import org.sdmlib.logger.LogEntry;
 import org.sdmlib.models.classes.Association;
+import org.sdmlib.models.classes.Card;
 import org.sdmlib.models.classes.ClassModel;
 import org.sdmlib.models.classes.Clazz;
+import org.sdmlib.models.classes.DataType;
 import org.sdmlib.models.classes.Role;
-import org.sdmlib.models.classes.Role.R;
 import org.sdmlib.storyboards.KanbanEntry;
-import org.sdmlib.storyboards.LogEntry;
 import org.sdmlib.storyboards.Storyboard;
-import org.sdmlib.storyboards.StoryboardManager;
 import org.sdmlib.storyboards.StoryboardWall;
 
 public class ProjectBoard
 {
-   @Test
-   public void testProjectManagement()
-   {
-      Storyboard story = new Storyboard();
+   //   @Test
+   //   public void testProjectManagement()
+   //   {
+   //      Storyboard story = new Storyboard();
+   //
+   //      story = new Storyboard("test", "MiniTutorials");
+   //      story.setSprint("ToDo");
+   //      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 14:13:42", 0, 12, "high priority");
+   //      story.dumpHTML();
+   //      
+   //      story = new Storyboard("test", "FeatureList");
+   //      story.setSprint("ToDo");
+   //      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 14:25:42", 0, 6, "medium priority");
+   //      story.dumpHTML();
+   //      
+   //      story = new Storyboard("test", "POJO Code enhancer");
+   //      story.setSprint("ToDo");
+   //      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 22:25:42", 0, 6, "high priority");
+   //      story.dumpHTML();
+   //   }
+   //   
+   //   
+   //   @Test
+   //   public void testFeatures()
+   //   {
+   //      Storyboard story = new Storyboard();
+   //
+   //      story = new Storyboard("test", "GenerateTransitiveClosureGetter");
+   //      story.setSprint("Features");
+   //      story.add("This feature has been tested within <a href='StudyRight%20with%20assignments%20class%20generation.html'>StudyRight with assignments class generation</a> ");
+   //      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 14:05:42", 0, 4, "high priority");
+   //      story.addLogEntry(R.DONE, "zuendorf", "11.03.2014 12:55:42", 4, 0, "works like a charm");
+   //      story.addLogEntry(R.DONE, "zuendorf", "11.03.2014 18:12:42", 2, 0, "well some legacy problems had to be fixed");
+   //      story.dumpHTML();
+   //      
+   //   }
+   //
+   //   
+   //   @Test
+   //   public void testExtendStoryboardByAddToDoMethod()
+   //   {
+   //      Storyboard storyboard = new Storyboard("test");
+   //      
+   //      storyboard.add("Start situation: ",
+   //         DONE, "zuendorf", "01.11.2012 12:33:42", 1, 0);
+   //      
+   //      storyboard.dumpHTML();
+   //   }
+   //
+   //   public static final String MODELING = "modeling";
+   //   public static final String ACTIVE = "active";
+   //   public static final String DONE = "done";
+   //   public static final String IMPLEMENTATION = "implementation";
+   //   public static final String BACKLOG = "backlog";
+   //
+   //   @Test
+   //   public void testTodoEntries()
+   //   {
+   //      Storyboard story = new Storyboard("test");
+   //      
+   //      story.add("Internal things.");
+   //      
+   //      story.add("It should be possible to add todo entries to the kanban board without adding a storyboard for them. "
+   //         , DONE, "zuendorf", "08.10.2013 13:30:42", 1, 0);
+   //      
+   //      story.addToDo("ExtendStoryboardByAddToDoMethod", DONE, "zuendorf", "21.08.2012 17:53:42", 2, 0)
+   //      .linkToTest("test", this.getClass().getName());
+   //      
+   //      story.dumpHTML();
+   //      
+   //   }
+   //   
 
-      story = new Storyboard("test", "MiniTutorials");
-      story.setSprint("ToDo");
-      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 14:13:42", 0, 12, "high priority");
-      story.dumpHTML();
-      
-      story = new Storyboard("test", "FeatureList");
-      story.setSprint("ToDo");
-      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 14:25:42", 0, 6, "medium priority");
-      story.dumpHTML();
-      
-      story = new Storyboard("test", "POJO Code enhancer");
-      story.setSprint("ToDo");
-      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 22:25:42", 0, 6, "high priority");
-      story.dumpHTML();
-   }
-   
-   
-   @Test
-   public void testFeatures()
-   {
-      Storyboard story = new Storyboard();
-
-      story = new Storyboard("test", "GenerateTransitiveClosureGetter");
-      story.setSprint("Features");
-      story.add("This feature has been tested within <a href='StudyRight%20with%20assignments%20class%20generation.html'>StudyRight with assignments class generation</a> ");
-      story.addLogEntry(BACKLOG, "zuendorf", "06.03.2014 14:05:42", 0, 4, "high priority");
-      story.addLogEntry(R.DONE, "zuendorf", "11.03.2014 12:55:42", 4, 0, "works like a charm");
-      story.addLogEntry(R.DONE, "zuendorf", "11.03.2014 18:12:42", 2, 0, "well some legacy problems had to be fixed");
-      story.dumpHTML();
-      
-   }
-
-   
-   @Test
-   public void testExtendStoryboardByAddToDoMethod()
-   {
-      Storyboard storyboard = new Storyboard("test");
-      
-      storyboard.add("Start situation: ",
-         DONE, "zuendorf", "01.11.2012 12:33:42", 1, 0);
-      
-      storyboard.dumpHTML();
-   }
-
-   public static final String MODELING = "modeling";
-   public static final String ACTIVE = "active";
-   public static final String DONE = "done";
-   public static final String IMPLEMENTATION = "implementation";
-   public static final String BACKLOG = "backlog";
-
-   @Test
-   public void testTodoEntries()
-   {
-      Storyboard story = new Storyboard("test");
-      
-      story.add("Internal things.");
-      
-      story.add("It should be possible to add todo entries to the kanban board without adding a storyboard for them. "
-         , DONE, "zuendorf", "08.10.2013 13:30:42", 1, 0);
-      
-      story.addToDo("ExtendStoryboardByAddToDoMethod", DONE, "zuendorf", "21.08.2012 17:53:42", 2, 0)
-      .linkToTest("test", this.getClass().getName());
-      
-      story.dumpHTML();
-      
-   }
-   
-   
    @Test
    public void testStoryboardInfrastructure()
    {
-      // file:///C:/Users/zuendorf/eclipseworkspaces/indigo/SDMLib/doc/StoryboardInfrastructure.html
-      Storyboard storyboard = new Storyboard();
-      
-      storyboard.setProjectName("SDMLibProject");
-      
-      storyboard.setSprint("Sprint.001.Booting");
-      
-      storyboard.add("This storyboard tests the storyboard infrastructure. ");
-      storyboard.addStep("At first creating the html file just with text should work. ");
-      storyboard.addStep("Next we need to create some class model. This will be done in a parallel activity.");
-      storyboard.addStep("With the class model we create an object model and try to dump it here.");
-      storyboard.addStep("Well, dumping the class model would be great, either.");
-
-      storyboard.add("need to restructure design: logentries shall be direct kids of kanbanentries. \n" +
-            "(has been below phase entries before.)\n" +
-            "phase entries will be used for planning, in future");
-      
+      //      Storyboard storyboard = new Storyboard();
+      //      
+      //      storyboard.setProjectName("SDMLibProject");
+      //      
+      //      storyboard.setSprint("Sprint.001.Booting");
+      //      
+      //      storyboard.add("This storyboard tests the storyboard infrastructure. ");
+      //      storyboard.addStep("At first creating the html file just with text should work. ");
+      //      storyboard.addStep("Next we need to create some class model. This will be done in a parallel activity.");
+      //      storyboard.addStep("With the class model we create an object model and try to dump it here.");
+      //      storyboard.addStep("Well, dumping the class model would be great, either.");
+      //
+      //      storyboard.add("need to restructure design: logentries shall be direct kids of kanbanentries. \n" +
+      //            "(has been below phase entries before.)\n" +
+      //            "phase entries will be used for planning, in future");
+      //      
       ClassModel model = new ClassModel("org.sdmlib.storyboards"); 
-      
-      Clazz kanbanEntryClass = new Clazz("org.sdmlib.storyboards.KanbanEntry", 
-         "oldNoOfLogEntries", R.INT, 
-         "phases", R.STRING);
 
-      Clazz logEntryClass = new Clazz("org.sdmlib.storyboards.LogEntry");
-      
+      Clazz kanbanEntryClass = model.createClazz("KanbanEntry")
+            .withAttribute("oldNoOfLogEntries", DataType.INT)  
+            .withAttribute("phases", DataType.STRING);
+
+      Clazz logEntryClass = model.createClazz("LogEntry");
+
       new Association()
-      .withSource("kanbanEntry", kanbanEntryClass, R.ONE, Role.AGGREGATION)
-      .withTarget("logEntries", logEntryClass, R.MANY);
-      
-      // Clazz phaseEntryClass = kanbanEntryClass.createClassAndAssoc(PhaseEntry.class.getName(), "phaseEntries", R.MANY, "kanbanEntry", R.ONE);
-      
-      Clazz storyboardWallClass = model.createClazz("StoryboardWall");
-      
-      Clazz storyboardClass = model.createClazz(Storyboard.class.getName(), 
-               "rootDir", R.STRING,
-               "stepCounter", R.INT, 
-               "stepDoneCounter", R.INT
-                  );
-      
-      storyboardWallClass.withAssoc(storyboardClass, "storyboard", R.ONE, "wall", R.ONE);
-      
-      Clazz storyboardStepClass = storyboardClass.createClassAndAssoc("StoryboardStep", "storyboardSteps", R.MANY, "storyboard", R.ONE)
-            .withAttributes("text", R.STRING);
-      
-      storyboard.addClassDiagram(model);
-      
-      model.generate("src", "srchelpers");
+      .withSource(kanbanEntryClass, "kanbanEntry", Card.ONE, Role.AGGREGATION)
+      .withTarget(logEntryClass, "logEntries", Card.MANY);
 
-      storyboard.add(" Editing the log entries works now fine as part of the add method. " , 
-            DONE, "zuendorf", "07.05.2012 23:36:42", 0, 0);
+      // Clazz phaseEntryClass = kanbanEntryClass.createClassAndAssoc(PhaseEntry.class.getName(), "phaseEntries", R.MANY, "kanbanEntry", R.ONE);
+
+      Clazz storyboardWallClass = model.createClazz("StoryboardWall");
+
+      Clazz storyboardClass = model.createClazz(Storyboard.class.getName()) 
+            .withAttribute("rootDir", DataType.STRING)
+            .withAttribute("stepCounter", DataType.INT) 
+            .withAttribute("stepDoneCounter", DataType.INT);
+
+      storyboardWallClass.withAssoc(storyboardClass, "storyboard", Card.ONE, "wall", Card.ONE);
+
+      Clazz storyboardStepClass = model.createClazz("StoryboardStep")
+            .withAttribute("text", DataType.STRING);
+
+      storyboardClass.withAssoc(storyboardStepClass, "storyboardSteps", Card.MANY, "storyboard", Card.ONE);
+
+      //      storyboard.addClassDiagram(model);
+
+      model.getGenerator().withShowDiff(true);
       
-      storyboard.add("Seems that we have solved the problem with the sorting of log entries after loading. " , 
-         DONE, "zuendorf", "19.05.2012 19:22:42", 1, 0);
-      
-      storyboard.addStep("Show some internals");
-      
-      storyboard.add("Internally, the class model looks like:");
-      
-      storyboard.addObjectDiagram(model);
-      
-      storyboard.addLogEntry(R.DONE, "zuendorf", "24.02.2014 18:38:00", 1, 0, "resolved old style admin to new storyboard features.");
-      storyboard.addLogEntry(R.DONE, "stefan, zuendorf", "28.02.2014 18:31:42", 23, 0, "switched to open source charts.");
-   
-      storyboard.dumpHTML();
+      model.generate();
+
+      //      storyboard.add(" Editing the log entries works now fine as part of the add method. " , 
+      //            DONE, "zuendorf", "07.05.2012 23:36:42", 0, 0);
+      //      
+      //      storyboard.add("Seems that we have solved the problem with the sorting of log entries after loading. " , 
+      //         DONE, "zuendorf", "19.05.2012 19:22:42", 1, 0);
+      //      
+      //      storyboard.addStep("Show some internals");
+      //      
+      //      storyboard.add("Internally, the class model looks like:");
+      //      
+      //      storyboard.addObjectDiagram(model);
+      //      
+      //      storyboard.addLogEntry(R.DONE, "zuendorf", "24.02.2014 18:38:00", 1, 0, "resolved old style admin to new storyboard features.");
+      //      storyboard.addLogEntry(R.DONE, "stefan, zuendorf", "28.02.2014 18:31:42", 23, 0, "switched to open source charts.");
+      //   
+      //      storyboard.dumpHTML();
    }
-   
-   @Test
-   public void testStoryboardInfrastructureInternals()
-   {
-      Storyboard storyboard = new Storyboard("test");
-      
-      storyboard.add("Internals.");
-      
-      storyboard.add("Object diagram for storyboard and KanbanEntry:" , 
-         DONE, "zuendorf", "08.10.2013 13:22:42", 1, 0);
-      
-      StoryboardWall storyboardWall = new StoryboardWall().withStoryboard(storyboard);
-      
-      KanbanEntry parent = new KanbanEntry();
-      
-      KanbanEntry kanbanEntry = new KanbanEntry()
-      .withHoursRemaining(1)
-      .withHoursSpend(2)
-      .withLastDeveloper("zuendorf")
-      .withName("name")
-      .withPhase(DONE)
-      .withParent(parent);
-      
-      LinkedHashMap<String, LogEntry> newLogEntries = storyboard.getNewLogEntries();
-      for (LogEntry entry : newLogEntries.values())
-      {
-         kanbanEntry.addToLogEntries(entry);
-      }
-      
-      
-      
-      storyboard.addObjectDiagram(storyboardWall, kanbanEntry);
-      
-      
-      storyboard.dumpHTML();
-   }
+
+   //   @Test
+   //   public void testStoryboardInfrastructureInternals()
+   //   {
+   //      Storyboard storyboard = new Storyboard("test");
+   //      
+   //      storyboard.add("Internals.");
+   //      
+   //      storyboard.add("Object diagram for storyboard and KanbanEntry:" , 
+   //         DONE, "zuendorf", "08.10.2013 13:22:42", 1, 0);
+   //      
+   //      StoryboardWall storyboardWall = new StoryboardWall().withStoryboard(storyboard);
+   //      
+   //      KanbanEntry parent = new KanbanEntry();
+   //      
+   //      KanbanEntry kanbanEntry = new KanbanEntry()
+   //      .withHoursRemaining(1)
+   //      .withHoursSpend(2)
+   //      .withLastDeveloper("zuendorf")
+   //      .withName("name")
+   //      .withPhase(DONE)
+   //      .withParent(parent);
+   //      
+   //      LinkedHashMap<String, LogEntry> newLogEntries = storyboard.getNewLogEntries();
+   //      for (LogEntry entry : newLogEntries.values())
+   //      {
+   //         kanbanEntry.addToLogEntries(entry);
+   //      }
+   //      
+   //      
+   //      
+   //      storyboard.addObjectDiagram(storyboardWall, kanbanEntry);
+   //      
+   //      
+   //      storyboard.dumpHTML();
+   //   }
 }
 
 
