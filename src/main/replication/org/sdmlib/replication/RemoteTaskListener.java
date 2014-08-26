6a9edc25791acea95f69f36592b3b44bf4678acb
Christian Repik <christian.repik@gmail.com>
2014-08-26 15:48:03 +0200

    add Tasks to RemoteTaskListener

--- src/main/replication/org/sdmlib/replication/RemoteTaskListener.java
+++ src/main/replication/org/sdmlib/replication/RemoteTaskListener.java
@@ -2,13 +2,20 @@ package org.sdmlib.replication;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.HashMap;
 
 public abstract class RemoteTaskListener implements PropertyChangeListener
 {
    public static final String NEW = "NEW";
    protected SharedSpace sharedSpace;
+   private HashMap<String, Runnable> tasks;
    private boolean isInit;
 
+   public RemoteTaskListener()
+   {
+      tasks = new HashMap<String, Runnable>();
+   }
+   
    @Override
    public void propertyChange(PropertyChangeEvent evt)
    {
@@ -43,6 +50,29 @@ public abstract class RemoteTaskListener implements PropertyChangeListener
       }
    }
 
+   protected void handleTasks(PropertyChangeEvent evt)
+   {
+      if (evt.getNewValue() != null)
+      {
+         BoardTask task = (BoardTask) evt.getNewValue();
+         String name = task.getName();
+         runTask(name);
+      }
+   }
+   
+   public RemoteTaskListener withTask(String name, Runnable task) {
+      tasks.put(name, task);
+      return this;
+   }
+   
+   public void runTask(String name) {
+      Runnable task = tasks.get(name);
+      if(task != null) {
+         task.run();
+      } else {
+         throw new RuntimeException("No such task: " + name);
+      }
+   }
    
    public SharedSpace getSharedSpace()
    {
