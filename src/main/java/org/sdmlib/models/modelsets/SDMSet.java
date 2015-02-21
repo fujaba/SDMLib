8446d3d7dc615b453499d408dfeb2147dfbae9c0
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2015-02-21 21:00:39 +0100

    merged SDMSet and SDMSetBase into SDMSet

--- src/main/java/org/sdmlib/models/modelsets/SDMSet.java
+++ src/main/java/org/sdmlib/models/modelsets/SDMSet.java
@@ -1,6 +1,147 @@
 package org.sdmlib.models.modelsets;
 
-public abstract class SDMSet<T> extends SDMSetBase<T> implements ModelSet
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.sdmlib.CGUtil;
+
+import de.uniks.networkparser.gui.ItemList;
+
+
+public abstract class SDMSet<T> extends ItemList<T> implements ModelSet
 {
-	//FIXME CHECK FOR DELETE THIS CLASS
+   @Override
+   public String toString()
+   {
+      StringList stringList = new StringList();
+      
+      for (T elem : this)
+      {
+         stringList.add(elem.toString());
+      }
+      
+      return "(" + stringList.concat(", ") + ")";
+   }
+   
+   public <ST extends SDMSet> ST instanceOf(ST target)
+   {
+	   String className;
+	   ParameterizedType genericSuperclass = (ParameterizedType) target.getClass().getGenericSuperclass();
+	   if(genericSuperclass.getActualTypeArguments().length>0){
+		   className = genericSuperclass.getActualTypeArguments()[0].getTypeName();
+	   }else{
+	      className = target.getClass().getName();
+	      className = CGUtil.baseClassName(className, "Set");
+	   }
+      try
+      {
+         Class<?> targetClass = target.getClass().getClassLoader().loadClass(className);
+         for (T elem : this)
+         {
+            if (targetClass.isAssignableFrom(elem.getClass()))
+            {
+               target.add(elem);
+            }
+         }
+      }
+      catch (ClassNotFoundException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return target;
+   }
+   
+   public <ST extends SDMSet<T>> ST union(Collection<? extends T> other)
+   {
+      @SuppressWarnings("unchecked")
+      ST result = (ST) this.getNewInstance();
+      result.addAll(this);
+      result.addAll(other);
+      
+      return result;
+   }
+   
+   
+   public <ST extends SDMSet<T>> ST intersection(Collection<? extends T> other)
+   {
+      @SuppressWarnings("unchecked")
+      ST result = (ST) this.getNewInstance();
+      result.addAll(this);
+      result.retainAll(other);
+      return result;
+   }
+   
+   
+   @SuppressWarnings("unchecked")
+   public <ST extends SDMSet<T>> ST minus(Object other)
+   {
+      ST result = (ST) this.getNewInstance();
+      result.addAll(this);
+      
+      if (other instanceof Collection)
+      {
+         result.removeAll((Collection<?>) other);
+      }
+      else
+      {
+         result.remove(other);
+      }
+      
+      return result;
+   }
+
+   public <ST extends SDMSet<T>> ST has(Condition<T> condition)
+   {
+      @SuppressWarnings("unchecked")
+	ST result = (ST) this.getNewInstance();
+      result.addAll(this);
+      
+      for (T elem : this)
+      {
+         if ( ! condition.check(elem))
+         {
+            result.remove(elem);
+         }
+      };
+      return result;
+   }
+   
+//   @Override
+//   public AbstractList<T> clone() {
+//      return this.getNewInstance().with(this);
+//   }
+   
+   public Iterator<T> cloneIterator() {
+      return super.clone().iterator();
+   }
+   
+   @Override
+   public SDMSet<T> getNewInstance() 
+   {
+      SDMSet<T> result = null;
+      try
+      {
+         result = this.getClass().newInstance();
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+      return result;
+   }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public SDMSet<T> with(Object... values) {
+      for(Object item : values){
+         this.add((T) item);
+      }
+      return this;
+   }
+	@Override
+	public boolean remove(Object value) {
+		return removeByObject(value) >= 0;
+	}
 }
--- src/test/java/org/sdmlib/examples/studyrightWithAssignments/model/util/CreatorCreator.java
+++ src/test/java/org/sdmlib/examples/studyrightWithAssignments/model/util/CreatorCreator.java
@@ -9,16 +9,6 @@ public class CreatorCreator{
    {
       JsonIdMap jsonIdMap = (JsonIdMap) new SDMLibJsonIdMap().withSessionId(sessionID);
       
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.UniversityCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.UniversityPOCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.StudentCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.StudentPOCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.RoomCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.RoomPOCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.AssignmentCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.AssignmentPOCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.TeachingAssistantCreator());
-      jsonIdMap.withCreator(new org.sdmlib.examples.studyrightWithAssignments.model.util.TeachingAssistantPOCreator());
 
       return jsonIdMap;
    }
