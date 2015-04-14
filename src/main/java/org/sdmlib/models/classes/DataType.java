90cc5af566fcef19cc3ee14a17ea0adda3da6d6e
Olaf Gunkel <olaf.gunkel@cs.uni-kassel.de>
2015-04-14 13:55:23 +0200

    java 1.7 compatibility restored

--- src/main/java/org/sdmlib/models/classes/DataType.java
+++ src/main/java/org/sdmlib/models/classes/DataType.java
@@ -2,75 +2,76 @@ package org.sdmlib.models.classes;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-public class DataType 
+public class DataType
 {
 
+   private static ConcurrentHashMap<String, DataType> instances = new ConcurrentHashMap<>();
 
-	private static ConcurrentHashMap<String, DataType> instances = new ConcurrentHashMap<>();
-	
+   public static synchronized DataType getInstance(String key)
+   {
+      DataType result = instances.get(key);
 
-	public static DataType getInstance(String value)
-	{
-		return instances.computeIfAbsent(value,DataType::new);
-	}
+      if (result == null)
+      {
+         result = new DataType(key);
+         instances.put(key, result);
 
-	public static final DataType VOID =  getInstance("void");
-	public static final DataType INT =  getInstance("int");
-	public static final DataType LONG =  getInstance("long");
-	public static final DataType DOUBLE =  getInstance("double");
-	public static final DataType STRING =  getInstance("String");
-	public static final DataType BOOLEAN =  getInstance("boolean");
-	public static final DataType OBJECT =  getInstance("Object");
+      }
+      return result;
 
-	private String value;
+   }
 
-	private DataType ( String value )
-	{
-		this.value = value;
-	}
+   public static final DataType VOID = getInstance("void");
+   public static final DataType INT = getInstance("int");
+   public static final DataType LONG = getInstance("long");
+   public static final DataType DOUBLE = getInstance("double");
+   public static final DataType STRING = getInstance("String");
+   public static final DataType BOOLEAN = getInstance("boolean");
+   public static final DataType OBJECT = getInstance("Object");
 
-	public String getValue()
-	{
-		return value;
-	}
+   private String value;
 
+   private DataType ( String value )
+   {
+      this.value = value;
+   }
 
+   public String getValue()
+   {
+      return value;
+   }
 
-	public static DataType ref(String value)
-	{
-		return getInstance(value);
-	}
+   public static DataType ref(String value)
+   {
+      return getInstance(value);
+   }
 
-	public static DataType ref(Class<?> value)
-	{
-		return getInstance(value.getName().replace("$", "."));
-	}
+   public static DataType ref(Class<?> value)
+   {
+      return getInstance(value.getName().replace("$", "."));
+   }
 
-	public static DataType ref(Clazz value)
-	{
-		return getInstance(value.getFullName());
-	}
+   public static DataType ref(Clazz value)
+   {
+      return getInstance(value.getFullName());
+   }
 
-	public static DataType ref(Enumeration value)
-	{
-		return getInstance(value.getFullName());
-	}
+   public static DataType ref(Enumeration value)
+   {
+      return getInstance(value.getFullName());
+   }
 
-	@Override
-	public String toString()
-	{
-		if ("void int long double String boolean Object".indexOf(this.value) >= 0)
-		{
-			return "DataType." + value.toUpperCase();
-		}
-		else
-		{
-			return "DataType.ref(\"" + value + "\")";
-		}
-	}
-
-
-
-
+   @Override
+   public String toString()
+   {
+      if ("void int long double String boolean Object".indexOf(this.value) >= 0)
+      {
+         return "DataType." + value.toUpperCase();
+      }
+      else
+      {
+         return "DataType.ref(\"" + value + "\")";
+      }
+   }
 
 }
