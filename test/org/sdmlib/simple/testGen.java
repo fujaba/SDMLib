cd1eb2299af25b26796eaea0244fb1a28f6e03ed
Stefan Lindel <slin@cs.uni-kassel.de>
2015-12-15 10:09:11 +0100

    add first test

---
+++ test/org/sdmlib/simple/testGen.java
@@ -0,0 +1,18 @@
+package org.sdmlib.simple;
+
+import org.junit.Test;
+import org.sdmlib.models.classes.ClassModel;
+
+import de.uniks.networkparser.graph.Clazz;
+import de.uniks.networkparser.graph.DataType;
+
+public class testGen {
+
+	@Test
+	public void testModel() {
+		ClassModel model=new ClassModel("org.sdmlib.simple.model");
+		Clazz person = model.createClazz("Person");
+		person.createAttribute("name", DataType.STRING);
+		model.generate("test");
+	}
+}
