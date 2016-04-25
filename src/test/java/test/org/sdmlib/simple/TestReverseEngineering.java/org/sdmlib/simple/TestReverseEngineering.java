2d4b3e4242fdc625c70e157ea2c321a5e3cfb1e3
Stefan Lindel <slin@cs.uni-kassel.de>
2016-04-25 22:03:54 +0200

    start to fix Reverse Engineering

---
+++ src/test/java/test/org/sdmlib/simple/TestReverseEngineering.java/org/sdmlib/simple/TestReverseEngineering.java
@@ -0,0 +1,9 @@
+package org.sdmlib.simple;
+
+public  class TestReverseEngineering
+{
+      @Test
+      public void genModel() {
+      	ClassModel clazzModel = new ClassModel("i.love.sdmlib");
+      }
+}
