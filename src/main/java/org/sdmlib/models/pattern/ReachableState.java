387b585edcf56efe0798fb63d2fea1b6dbd03e0d
Philipp Copei <philipp.copei@gmx.net>
2016-10-27 15:54:35 +0200

    adjusted firePropertyChange for ReachableState

--- src/main/java/org/sdmlib/models/pattern/ReachableState.java
+++ src/main/java/org/sdmlib/models/pattern/ReachableState.java
@@ -742,4 +742,13 @@ public class ReachableState implements PropertyChangeInterface, SendableEntity
    {
       return failureState;
    }
+   
+   public boolean firePropertyChange(String propertyName, Object oldValue, Object newValue)
+   {
+	   if (listeners != null) {
+		   listeners.firePropertyChange(propertyName, oldValue, newValue);
+		   return true;
+	   }
+	   return false;
+   }
 }
