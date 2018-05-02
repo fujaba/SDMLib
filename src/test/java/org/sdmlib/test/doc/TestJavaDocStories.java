18a1f41a6c2108ce6c9b23f8a7d607e4c4f1fdac
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2018-05-02 12:18:32 +0200

    javadoc stuff

--- src/test/java/org/sdmlib/test/doc/TestJavaDocStories.java
+++ src/test/java/org/sdmlib/test/doc/TestJavaDocStories.java
@@ -8,6 +8,7 @@ import org.sdmlib.storyboards.MikadoLog;
 import org.sdmlib.storyboards.Storyboard;
 import org.sdmlib.test.examples.studyrightWithAssignments.model.University;
 
+import java.io.File;
 import java.util.LinkedHashMap;
 
 public class TestJavaDocStories
@@ -15,6 +16,69 @@ public class TestJavaDocStories
    public static void main(String[] args)
    {
       System.out.println("Hello");
+   }
+
+   /**
+    *
+    * <p>Storyboard OldJavaDocStoriesMikadoPlan</p>
+    * <img src="doc-files/OldJavaDocStoriesMikadoPlanStep0.png" alt="OldJavaDocStoriesMikadoPlanStep0.png">
+    * <p>Start: open goals:</p>
+    * <img src="doc-files/OldJavaDocStoriesMikadoPlanStep2.png" alt="OldJavaDocStoriesMikadoPlanStep2.png">
+    * <p>object diagrams as image via graphviz: {@link Storyboard#addObjectDiagramViaGraphViz}</p>
+    * <p>generate charts as png: {@link Storyboard#addAsImage}</p>
+    * <p><a name = 'step_1'>Step 1: closed goals:</a></p>
+    * <img src="doc-files/OldJavaDocStoriesMikadoPlanStep6.png" alt="OldJavaDocStoriesMikadoPlanStep6.png">
+    * <p>Check: ensure screen dump exists false</p>
+    */
+   @Test
+   public void testOldJavaDocStoriesMikadoPlan()
+   {
+      String mikadoText = "" +
+              "- Goal                  description:                               parents:   \n" +
+              "  main:                 \"JavaDoc Stories\"                        null       \n" +
+              "  storyBoardExtension:  \"add gen javadoc methods to storyBoard\"  main       \n" +
+              "  generateJavaDocText:  \"generate javadoc text\"                  main       \n" +
+              "  insertJavaDocInClass: \"insert javadoc in class\"                main       \n" +
+              "  insertStoryInTest:    \"insert Story as TestMethod javadoc\"     insertJavaDocInClass       \n" +
+              "  genSeeRefsAsLinks:    \"replace see ../xy.java by see xy\"       insertJavaDocInClass       \n" +
+              "  graphVizObjectDiags:  \"object diagrams as image via graphviz\"               main       \n" +
+              "  codeGen4OtherRole:    \"extend Creator for other role\"               main       \n" +
+              "  graphVizClazzDiags:   \"class diagrams as image\"                main       \n" +
+              "  graphVizPatternDiags:   \"pattern diagrams as image\"                main       \n" +
+              "  chartsAsPng:          \"generate charts as png\"                 main       \n" +
+              "  objectTables:         \"have object tables in javadocs\"         main       \n" +
+              "  \n" +
+              "- mikadoLog: MikadoLog                                                        \n" +
+              "  mainGoal: main                                                              \n" +
+              "  \n" +
+              "- LogEntry  goal:              date:                           hoursDone: hoursRemaining:  parent:        \n" +
+              "  l1:       main               2018-04-03T12:15:00+01:00        0           4              mikadoLog      \n" +
+              "  l2:       genSeeRefsAsLinks  2018-04-03T15:00:00+01:00        2.5         0              mikadoLog      \n" +
+              "  l3:       insertStoryInTest  2018-04-03T20:00:00+01:00        2.5         0              mikadoLog      \n" +
+              "  l4:       insertJavaDocInClass  2018-04-03T20:00:00+01:00     1           0              mikadoLog      \n" +
+              "  l5:       generateJavaDocText  2018-04-03T20:00:00+01:00      1           0              mikadoLog      \n" +
+              "  l6:       storyBoardExtension  2018-04-03T20:00:00+01:00      1           0              mikadoLog      \n" +
+              "  l7:       graphVizObjectDiags  2018-04-08T17:00:00+01:00      2           0              mikadoLog      \n" +
+              "  l8:       graphVizClazzDiags   2018-04-08T17:10:00+01:00      2           3              mikadoLog      \n" +
+              "  l9:       chartsAsPng          2018-04-08T17:20:00+01:00      2           0              mikadoLog      \n" +
+              "  l10:      objectTables         2018-04-22T01:47:00+01:00      4           0              mikadoLog      \n" +
+              "";
+
+      YamlIdMap idMap = new YamlIdMap(Goal.class.getPackage().getName());
+      Goal root = (Goal) idMap.decode(mikadoText);
+      MikadoLog mikadoLog = (MikadoLog) idMap.getObject("mikadoLog");
+      Storyboard story = new Storyboard().withDocDirName("doc/internal");
+      story.addAsImage(mikadoLog.burnDownChart(), 910, 651);
+      Goal done = root.clipDone();
+      story.addStep("open goals:");
+      story.addObjectDiagramAsImage(root, 901, 451);
+      story.add("object diagrams as image via graphviz: {@link " + Storyboard.class.getSimpleName() + "#addObjectDiagramViaGraphViz"  +  "}");
+      story.add("generate charts as png: {@link " + Storyboard.class.getSimpleName() + "#addAsImage"  +  "}");
+      story.addStep("closed goals:");
+      story.addObjectDiagramAsImage(done, 950, 600);
+      story.assertTrue("ensure screen dump exists", new File("doc-files/OldJavaDocStoriesMikadoPlanStep0.png").exists());
+      story.dumpHTML();
+
    }
 
    /**
@@ -111,8 +175,8 @@ public class TestJavaDocStories
 
       story.addPreformatted(map.toString());
 
-      story.dumpJavaDoc(Yamler.class.getName());
-      story.dumpJavaDoc(Yamler.class.getName(), "decode(String)");
+      // story.dumpJavaDoc(Yamler.class.getName());
+      // story.dumpJavaDoc(Yamler.class.getName(), "decode(String)");
 
       story.dumpHTML();
    }
